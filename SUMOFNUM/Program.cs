59ae2cba1c17755df2b5406a4c6702c5dfa9cebc
abhijith kr <abhijithkr3@outlook.com>
2021-07-25 20:35:19 +0530

    Add project files.

---
+++ SUMOFNUM/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SUMOFNUM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
